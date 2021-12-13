31ad5514992c5bea59cd080205221e717d34422b
dylan <dylan.ferrari@epitech.eu>
2021-12-13 16:40:18 +0100

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ from matplotlib.figure import Figure
 import seaborn as sns
 
 
-st.set_page_config(page_title="Heart Disease", page_icon="https://www.freeiconspng.com/thumbs/heart-png/heart-png-15.png", layout='centered', initial_sidebar_state="collapsed")
+st.set_page_config(page_title="Finance platform", page_icon="https://www.freeiconspng.com/thumbs/heart-png/heart-png-15.png", layout='centered', initial_sidebar_state="collapsed")
 
 def main():
     # title
